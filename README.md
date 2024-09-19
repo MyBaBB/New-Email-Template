d371c63c684a96400f121e8b63ae15dfb65e5c30
Brett Baker <GitBaBB@MyBaBB.com>
2024-09-19 07:31:55 -0700

    first commit

---
+++ README.md
@@ -0,0 +1,8 @@
+<!-- note to self just push to deploy to github pages -->
+
+# My_gmail_Template
+
+I use this as an example template for a build,
+(https://picsum.photos/width/Height) is a placeholder for an image,
+
+![Image](emailReadme.png)
---
+++ src/TestEmail.jsx
@@ -0,0 +1,268 @@
+// eslint-disable-next-line no-unused-vars
+import React from "react";
+ 
+ 
+const EmailTemplate = () => (
+  <div className="Fixed w-screen h-screen top-0 left-0  ">
+    <div className="">
+ 
+        <title>Web Developer Friend</title>
+        <style>{`
+          @import url('https://fonts.googleapis.com/css2?family=Times+New+Roman&display=swap');
+        `}</style>
+          </div>
+          <div>
+        <div>
+          <div>
+            <div
+          className=" "
+          style={{
+            background: "radial-gradient(circle,  #aac4ea , #4980d2)",
+          }}
+            > 
+            <div className="bg-slate-900 relative pb-4 pt-1 flex-col rounded-xl max-w-[620px] m-auto ">
+          <div >
+            <p className="text-center text-xl text-green-400 mx-8 " 
+          style={{border: '2px solid transparent',
+              fontFamily: 'georgia, serif',
+
+          }}>
+             E-Mail Template Example
+           </p>
+            <p className="text-center text-xl text-green-400 mx-8 " 
+          style={{border: '2px solid transparent',
+              fontFamily: 'georgia, serif',
+
+          }}>
+            For Speed, SEO, and Visuals
+           </p>
+            <p className="text-center text-xl text-green-400 mx-8 " 
+          style={{border: '2px solid transparent',
+              fontFamily: 'georgia, serif',
+
+          }}>
+             Enhance Your Wordpress
+           </p>
+           </div>
+             
+            <div
+              className="relative m-auto mx-8 max-w-[600px] flex-col rounded-xl py-4 text-blue-950"
+              style={{
+            border: "4px solid #0f172a",
+            outline: "1px solid aliceblue",
+            fontFamily: "georgia, serif",
+            background: "radial-gradient(circle, aliceblue,  #334155)", 
+              }}
+            >
+              <section className="topSection">
+            <div className="topSectionDvi relative m-auto flex w-fit items-center justify-center rounded-2xl bg-slate-600 p-2">
+              <div className="relative m-auto flex w-fit text-amber-200">
+                <a
+              href="https://mybabb.github.io/About_MyBaBB/"
+              style={{
+                hover: "underline",
+                color: "white",
+                textDecoration: "none",
+                
+              }}
+                >
+               <span className="clip text-[#7ad1ee]">
+                
+                 🔗
+                     My Services 
+               👈 </span> 
+                </a>
+              </div>
+
+              {/* <a href="https://github.com/MyBaBB">
+              <img src="https://fastweb.mybabb.com/images/githubkitty2.png"
+                alt="github"
+                width="30" class="toot" /></a>
+                
+            <a href="https://www.facebook.com/profile.php?id=61551238172159">
+              <img src="https://fastweb.mybabb.com/images/white-facebook.png"
+                alt="FB"
+                width="30" class="toot" /></a>
+
+            <a href="https://www.youtube.com/channel/UC4ebMYgUhUEkkQm8HDSmj2g">
+              <img src="https://fastweb.mybabb.com/images//white-youtube.png"
+                alt="youtube"
+                width="30" class="toot" /></a>
+
+            <a href="https://www.linkedin.com/in/mybabb-customweb/">
+              <img src="https://fastweb.mybabb.com/images//white-linkedin.png"
+                alt="linkedin"
+                width="30" class="toot" /></a>
+
+            <a href="https://www.instagram.com/mybabbweb/">
+              <img src="https://fastweb.mybabb.com/images//white-instagram.png"
+                alt="Instagram"
+                width="30" class="toot" /></a>
+             */}
+                  </div>
+                </section>
+                <div className="text-center text-red-950 font-bold text-[12px]">
+
+                <span className="block py-[2px]">
+                🔹Custom Websites
+                  </span>
+              
+                <span className="block py-[2px]">
+                🔸Customized Parts
+                  </span>
+
+                  <span className="block py-[2px]">
+                  🔹No job too Big or Small
+                  </span>
+                  <span className="block py-[2px]">
+                  🔸Front-End React.Js
+                  </span>
+
+                  <span className="block py-[2px]">
+                  🔹Web Developer
+                  </span>
+                 
+                 
+                 
+                  <span className="block py-[2px]">
+                  🔸UI/UX Developer
+                                      </span>
+
+                  <span className="block py-[2px]">
+                  🔹Graphic Artist
+                  </span>
+                  
+                  <span className="block py-[2px]">
+                  🔸I save you time
+                  </span>
+                  
+                  <span className="block py-[2px]">
+                  🔹Consultation
+                  </span>
+                  
+                  <span className="block py-[2px]">
+                  🔸&quot;Buy Me a Coffee&quot;
+                  </span>
+                  
+                  
+                </div>
+              </div>
+
+              <div >
+            <p className="text-center text-xl text-green-400 mx-8 " 
+          style={{border: '2px solid transparent',
+              fontFamily: 'georgia, serif',
+
+          }}>
+             Modify This Email Template
+           </p>
+            <p className="text-center text-xl text-green-400 mx-8 " 
+          style={{border: '2px solid transparent',
+              fontFamily: 'georgia, serif',
+
+          }}>
+             With Buttons to Your Website
+           </p>
+            <p className="text-center text-xl text-green-400 mx-8 " 
+          style={{border: '2px solid transparent',
+              fontFamily: 'georgia, serif',
+
+          }}>
+             Or Custom Landing Page
+           </p>
+           </div>
+
+
+
+              <div
+                className="relative m-auto mt-2 mx-8 max-w-[600px] flex-row items-center justify-center rounded-xl bg-[#4980d2] py-4"
+                style={{ border: "4px solid #0f172a",
+                  outline: "1px solid aliceblue",
+                  fontFamily: "georgia, serif",
+                  background: "radial-gradient(circle, aliceblue,  #334155)", }}
+              >
+ <div className="flex justify-center w-fit m-auto mt-2  "
+                style={{border:'2px solid transparent '}}>
+                  <a href="https://resume-hub.mybabb.com/">
+                    <img
+                      src="https://fastweb.mybabb.com/images/Octopussy2.gif"
+                      alt="Octopus Portfolio Image" className="rounded-xl"
+                    />
+                  </a>
+                </div>
+
+
+
+
+
+                <div className="flex justify-center flex-col m-auto mt-4 w-fit" 
+                style={{border:'2px solid transparent ' }}>
+                  <a href="https://mybabb.com">
+                    
+                      <img
+                        src="https://fastweb.mybabb.com/images/NewMew-paperclip-300px.jpg"
+                        alt="My Portfolio Image"
+                        style={{border:'2px solid transparent ' ,
+                          borderRadius:' 18px '
+                  }} 
+                      />
+                  
+                  </a>
+
+
+
+
+
+
+
+                  
+                  
+
+
+           
+                  </div>
+           
+              
+               
+              </div>
+              <p className="text-center text-xl text-green-400 mx-8 " 
+          style={{border: '2px solid transparent',
+              fontFamily: 'georgia, serif',
+
+          }}>
+             Combine this template with a  
+           </p>
+              <p className="text-center text-xl text-green-400 mx-8 " 
+          style={{border: '2px solid transparent',
+              fontFamily: 'georgia, serif',
+
+          }}>
+              Fast Loading Landing Page  
+           </p>
+              <p className="text-center text-xl text-green-400 mx-8 " 
+          style={{border: '2px solid transparent',
+              fontFamily: 'georgia, serif',
+
+          }}>
+             Search Engine Optimized (SEO)  
+           </p>
+            
+            
+            </div>
+
+            <div className="fixed bottom-0  right-8 w-fit m-auto mt-2
+                                rounded-xl py-[2px] px-[4px]   ">
+                <p className="text-blue-200 ">
+              &lt;&lt;&lt;&copy;&nbsp;<span className="text-orange-400">Brett&apos;s</span>&nbsp;8/2023 &gt;&gt;&gt;
+               </p>
+                   </div>
+          </div>
+          
+        </div>
+        
+      </div>
+    </div>
+    
+  </div>
+);
+export default EmailTemplate;
